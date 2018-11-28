ff6a861b5f58966878aba56826d6b2a0b80a8bdf
wawa <xiaozhang0210@hotmail.com>
2018-11-28 16:29:24 +0800

    init

---
+++ consoledemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace consoledemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.WriteLine("Hello World, the current time is :"+DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss ffff"));
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
